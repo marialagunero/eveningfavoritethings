937d6389072a4cc78aacbe42054fcb3fe3a5f881
Student <example@example.com>
2019-12-09 20:04:27 -0800

    add html, css, and scripts. Still working on code

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(form#favorite).submit(function(event) {
+    event.preventDefault();
+    var favorites = ["color", "drink", "animal", "food", "book"];
+
+    favorites.forEach(function(favorite) {
+      var userInput = $("input#" + favorite).val();
+      console.log(userInput);
+      $("#" + favorite).text(userInput).val();
+
+    });
+
+    $("#favorite").show();
+
+
+
+
+
+  });
+});
